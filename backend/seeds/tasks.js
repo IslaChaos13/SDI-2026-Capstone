8816bc3ef64b5131dd1e6beae06f87154dc82479
john l <yellowpen90000@gmail.com>
2026-07-13 10:26:03 -0500

    corrected tasks seed

--- backend/seeds/tasks.js
+++ backend/seeds/tasks.js
@@ -88,8 +88,8 @@ const airmanTasks = [
 
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
+  await knex('tasks').del()
+  await knex('tasks').insert([
     {id: 1, colName: 'rowValue1'},
     {id: 2, colName: 'rowValue2'},
     {id: 3, colName: 'rowValue3'}
--- backend/seeds/users.js
+++ backend/seeds/users.js
@@ -6,8 +6,8 @@
 
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('tasks').del()
-  await knex('tasks').insert([
+  await knex('').del()
+  await knex('').insert([
     {id: 1, colName: 'rowValue1'},
     {id: 2, colName: 'rowValue2'},
     {id: 3, colName: 'rowValue3'}
