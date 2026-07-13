e54d8113158644635fc06d1590972dbb6e389df7
vladboudreaux <vladboudreaux651@gmail.com>
2026-07-13 12:01:02 -0400

    baseroutes

--- backend/api.js
+++ backend/api.js
@@ -6,6 +6,15 @@ const knex = require('knex')(require('./knexfile.js')['development']);
 
 app.use(express.json());
 
+app.get('/db_status', async (req, res) => {
+   try {
+      await knex.raw('SELECT 1')
+      res.json({ status: 'ok' })
+   } catch (err) {
+      res.status(500).json({ error: 'DB connection failed', details: err.message })
+   }
+})
+
 app.get('/users', async (req, res) => {
    try {
       const [users] = await Promise.all([
@@ -22,6 +31,38 @@ app.get('/users', async (req, res) => {
       });
    }
 });
+
+app.get('/tasks', async (req, res) => {
+   try {
+      const [tasks] = await Promise.all([
+         knex('users').select('*')
+      ])
+
+      res.status(200).json({
+         tasks: tasks
+      })
+   } catch (err) {
+      res.status(500).json({
+         message: 'Failed to fetch data'
+      })
+   }
+})
+
+app.get('/directory', async (req, res) => {
+   try {
+      const [directory] = await Promise.all([
+         knex('directory').select('*')
+      ])
+
+      res.status(200).json({
+         directory: directory
+      })
+   } catch (err) {
+      res.status(500).json({
+         message: 'Failed to fetch data'
+      })
+   }
+})
 
 app.listen(PORT, () => {
    console.log(`Server running at http://localhost:${PORT}`);
