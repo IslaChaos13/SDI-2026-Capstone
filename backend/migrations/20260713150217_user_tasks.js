250cfa0aa7233ddce389fb0b23d9aeef2c6b0800
vladboudreaux <vladboudreaux651@gmail.com>
2026-07-13 11:06:01 -0400

    users, tasks, user_tasks

---
+++ backend/migrations/20260713150217_user_tasks.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('user_tasks', table => {
+        table.integer('user_id').references('id').inTable('users')
+        table.integer('task_id').references('id').inTable('tasks')
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('user_tasks')
+};
