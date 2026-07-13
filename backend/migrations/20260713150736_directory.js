c55bc6530e29c58a10dd9f6a163ce3f10f471452
Alex <deabenderfer@DESKTOP-JAPQJ39.localdomain>
2026-07-13 10:26:01 -0500

    Directory Migration

---
+++ backend/migrations/20260713150736_directory.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('directory', table => {
+    table.increments('id')
+    table.string('title')
+    table.string('link')
+    table.string('phone')
+    table.string('address')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('directory')
+};
