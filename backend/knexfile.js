14b386789217bdc5f4f3b5ffbbacc91c0fdbc914
AldebaranFox <aldebaranfox@gmail.com>
2026-07-13 09:38:20 -0500

    First migrations

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
       port:'5432',
       user:'postgres',
       password:'docker',
-      database:'Functions'
+      database:'WingmanDatabase'
     },
     migrations: {
       directory: './migrations'
---
+++ backend/migrations/20260713143109_users.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+   return knex.schema.createTable('users', table => {
+      table.increments('id');
+      table.bool('is_admin').notNullable();
+      table.string('rank');
+      table.string('first_name');
+      table.string('last_name');
+      table.string('email');
+      table.string('phone');
+      table.string('address');
+      table.string('avatar');
+      table.string('password');
+   });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+   return knex.schema.dropTableIfExists('users');
+};
