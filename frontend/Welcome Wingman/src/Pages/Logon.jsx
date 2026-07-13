6a2b249ba0bd665c806d2061017f6da7a3299e4b
Doster0 <darrion.oster@gmail.com>
2026-07-13 16:55:51 -0500

    finished nav bar with allowing for scalibility need to finish login watching videos on safe practices

---
+++ frontend/Welcome Wingman/src/Pages/Logon.jsx
@@ -0,0 +1,16 @@
+import '../css/Logon.css'
+
+function Logon() {
+    return(
+        <>
+            <div className="TextContainer">
+                <input type="text" placeholder="Username"></input>
+                <input type="password" placeholder="Password"></input>
+                <button>Enter</button>
+            </div>
+
+        </>
+    )
+}
+
+export default Logon
