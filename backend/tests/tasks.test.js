8364b832432d7334544a56633fc5eaf2d9cb6e17
john l <yellowpen90000@gmail.com>
2026-07-17 13:11:21 -0500

    vitests get routes

--- backend/tests/tasks.test.js
+++ backend/tests/tasks.test.js
@@ -1,15 +1,54 @@
-const request = require("supertest");
-const app = require("../api");
+const request = require('supertest')
 
-const express = require("express");
-const app = express();
-app.use(express.json());
+//Get routes - TEST
+describe('GET /db_status', () => {
+   it('returns database connection', async () => {
+      const response = await request('http://localhost:8000')
+         .get('/db_status')
 
-// routes
+      expect(response.status).toBe(200)
+      expect(response.body.status).toBe('ok')
+   })
+})
 
+describe('GET /tasks', () => {
+   it('returns the tasks', async () => {
+      const response = await request('http://localhost:8000')
+         .get('/tasks')
 
+      expect(response.status).toBe(200)
+      expect(Array.isArray(response.body.tasks)).toBe(true)
+   })
+})
 
+describe('GET /users', () => {
+   it('returns the users', async () => {
+      const response = await request('http://localhost:8000')
+         .get('/users')
 
+      expect(response.status).toBe(200)
+      expect(Array.isArray(response.body.users)).toBe(true)
+   })
+})
 
+describe('GET /directory', () => {
+   it('returns the directory', async () => {
+      const response = await request('http://localhost:8000')
+         .get('/directory')
 
-module.exports = app;
+      expect(response.status).toBe(200)
+      expect(Array.isArray(response.body.directory)).toBe(true)
+   })
+})
+
+describe('GET /user_tasks', () => {
+   it('returns the user tasks', async () => {
+      const response = await request('http://localhost:8000')
+         .get('/user_tasks')
+
+      expect(response.status).toBe(200)
+      expect(Array.isArray(response.body)).toBe(true)
+   })
+})
+
+//Post routes - TEST
