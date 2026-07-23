a394d657f5eae7d26aa3701af695a1710daf4c6d
vladboudreaux <vladboudreaux651@gmail.com>
2026-07-23 14:49:55 -0400

    added errorPage

--- frontend/Welcome Wingman/src/pages/ErrorPage.jsx
+++ frontend/Welcome Wingman/src/pages/ErrorPage.jsx
@@ -1,9 +1,25 @@
-function ErrorPage() {
-    return(
-        <>
-    
-        </>
-    )
+import '../css/ErrorPage.css'
+import { useNavigate } from 'react-router-dom'
+
+
+function ErrorPage({ message, onRetry }) {
+  const navigate = useNavigate()
+  return (
+    <div className="page error-page">
+      <div className="error-content">
+        <h1 className="title error-title">Something went wrong</h1>
+        <p className="error-message">
+          {message + " for assistance please contact 318-456-3666" || "We ran into an unexpected error. Please try again. Or contact 318-456-3666"}
+          <button onClick={() => navigate('/login')}> Login Here </button>
+        </p>
+        {onRetry && (
+          <button className="error-retry-btn" onClick={onRetry}>
+            Try Again
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
